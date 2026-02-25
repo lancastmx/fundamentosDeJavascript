10fe5efbf7783fdf3f9c1a9fe68528ba816155f8
lancast <angelprep92@gmail.com>
2026-02-25 11:56:11 -0600

    feat: ups era en otra rama

--- api/index.js
+++ api/index.js
@@ -1,11 +1,15 @@
 const express = require('express');
+const config = require('../config');
+
+const user = require('./components/user/network');
 const app = express();
-const port = 3000;
 
 app.get('/', (req, res) => {
   res.send('¡Hola! El servidor de Fundamentos está vivo.');
 });
 
-app.listen(port, () => {
-  console.log(`Servidor corriendo en http://localhost:${port}`);
+app.listen(config.api.port, () => {
+  console.log(
+    `Servidor de Fundamentos escuchando en el puerto ${config.api.port}`
+  );
 });
