d4b6b8d65295c7d2be20e3e0b494996f4772a22e
lancast <angelprep92@gmail.com>
2026-02-25 10:44:05 -0600

    test: probando husky y prettier

--- 01-vars.js
+++ 01-vars.js
@@ -1,3 +1,9 @@
-let nombre = "Juan";
 let edad = 25;
 let esEstudiante = true;
+
+console.log('Nombre:', nombre);
+console.log('Edad:', edad);
+console.log('¿Es estudiante?', esEstudiante);
+
+let nombre = 'Miyamoto Musashi';
+console.log(nombre);
